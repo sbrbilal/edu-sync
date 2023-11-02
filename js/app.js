f011bb5c17e3194adecf71cc8869a1adef6a4ebe
Sheikh Bilal Raza <sbrbilal77@gmail.com>
2023-11-02 12:30:37 +0000

    Sorting & Order Update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,28 @@ new Vue({
     el: '#app',
     data: {
         lessons: lessons,
-        cart: []
+        query: '',
+        cart: [],
+        sortList: [
+            {
+                value: 'Subject',
+                key: 'subject'
+            },
+            {
+                value: 'Location',
+                key: 'location'
+            },
+            {
+                value: 'Price',
+                key: 'price'
+            },
+            {
+                value: 'Spaces',
+                key: 'spaces'
+            },
+        ],
+        sortBy: 'subject',
+        sortOrder: 'asc'
     },
     methods: {
         addToCart(lessonId) {
@@ -19,6 +40,39 @@ new Vue({
                 }
                 this.lessons[lessonIndex].spaces--
             }
+        }
+    },
+    computed: {
+        filteredLessons() {
+            let lessons = this.lessons
+            if (this.query) {
+                const query = this.query.toLowerCase()
+                lessons = lessons.filter((lesson) => {
+                    return lesson.subject.toLowerCase().includes(query)
+                        || lesson.location.toLowerCase().includes(query)
+                })
+            }
+            return lessons.sort((lessonA, lessonB) => {
+                switch (this.sortBy) {
+                    case 'subject':
+                    case 'location':
+                        const attributeA = lessonA[this.sortBy].toLowerCase()
+                        const attributeB = lessonB[this.sortBy].toLowerCase()
+                        if (attributeA < attributeB) {
+                            return -1;
+                        }
+                        if (attributeA > attributeB) {
+                            return 1;
+                        }
+                        return 0;
+                        break;
+
+                    case 'price':
+                    case 'spaces':
+                        return lessonA[this.sortBy] - lessonB[this.sortBy]
+                        break;
+                }
+            })
         }
     }
 })
