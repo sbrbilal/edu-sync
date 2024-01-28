f57fe2ca3d95c74d1da4834b5177a989309c9534
Sheikh Bilal Raza <sbrbilal77@gmail.com>
2024-01-28 00:24:44 +0000

    Frontend Update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 new Vue({
     el: '#app',
     data: {
-        lessons: lessons,
+        backendLink: "https://edusync-env.eba-3c6tit6g.eu-west-2.elasticbeanstalk.com",
+        lessons: [],
         query: '',
         showCart: false,
         cart: [],
@@ -30,11 +31,37 @@ new Vue({
         sortBy: 'subject',
         sortOrder: 'asc'
     },
+    mounted() {
+        this.getLessons()
+    },
     methods: {
+        async getLessons() {
+            try {
+                const queryParams = {
+                    query: this.query,
+                    sortBy: this.sortBy,
+                    sortOrder: this.sortOrder,
+                }
+                const queryString = Object.entries(queryParams)
+                .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+                .join('&');
+                const response = await fetch(`${this.backendLink}/api/lessons?${queryString}`);
+                let fetchedLessons = await response.json();
+                this.cart.forEach(cartLesson => {
+                    const lessonIndex = fetchedLessons.findIndex((l) => l._id === cartLesson._id)
+                    if (lessonIndex != -1) {
+                        fetchedLessons[lessonIndex].spaces -= cartLesson.spaces
+                    }
+                });
+                this.lessons = fetchedLessons
+            } catch (error) {
+                console.error('Error fetching lessons:', error);
+            }
+        },
         addToCart(lessonId) {
-            const lessonIndex = this.lessons.findIndex((l) => l.id === lessonId)
+            const lessonIndex = this.lessons.findIndex((l) => l._id === lessonId)
             if (this.lessons[lessonIndex].spaces > 0) {
-                const cartIndex = this.cart.findIndex((l) => l.id === lessonId)
+                const cartIndex = this.cart.findIndex((l) => l._id === lessonId)
                 if (cartIndex === -1) {
                     this.cart.push({
                         ...this.lessons[lessonIndex],
@@ -47,59 +74,64 @@ new Vue({
             }
         },
         removeCartItem(cartIndex) {
-            const cartLesson = this.cart[cartIndex]
-            const lessonIndex = this.lessons.findIndex((l) => l.id === cartLesson.id)
-            this.lessons[lessonIndex].spaces += cartLesson.spaces
-            this.cart.splice(cartIndex, 1)
+            const lessonIndex = this.lessons.findIndex((l) => l._id === this.cart[cartIndex]._id)
+            this.lessons[lessonIndex].spaces++
+            this.cart[cartIndex].spaces--
+            if (this.cart[cartIndex].spaces == 0) {
+                this.cart.splice(cartIndex, 1)
+            }
         },
-        order() {
+        async order() {
             if (this.orderValidated) {
-                alert("Your order has been submitted!")
+                try {
+                    await fetch(`${this.backendLink}/api/orders`, {
+                        method: 'POST',
+                        headers: {
+                          'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            name: this.name,
+                            phone: this.phone,
+                            lessons: this.cart.map((lesson) => {
+                                return {
+                                    _id: lesson._id,
+                                    spaces: lesson.spaces
+                                }
+                            })
+                        })
+                    });
+
+                    await fetch(`${this.backendLink}/api/lessons`, {
+                        method: 'PUT',
+                        headers: {
+                          'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            lessons: this.cart
+                        })
+                    });
+
+                    this.showCart = false
+                    this.cart = []
+                    alert('Your order has been submitted!')
+                } catch (error) {
+                    alert('Something went wrong! Please try again later')
+                }
             }
         }
     },
-    computed: {
-        filteredLessons() {
-            let lessons = this.lessons
-            if (this.query) {
-                const query = this.query.toLowerCase()
-                lessons = lessons.filter((lesson) => {
-                    return lesson.subject.toLowerCase().includes(query)
-                        || lesson.location.toLowerCase().includes(query)
-                })
-            }
-            return lessons.sort((lessonA, lessonB) => {
-                switch (this.sortBy) {
-                    case 'subject':
-                    case 'location':
-                        let attributeA, attributeB
-                        if (this.sortOrder == 'asc') {
-                            attributeA = lessonA[this.sortBy].toLowerCase()
-                            attributeB = lessonB[this.sortBy].toLowerCase()
-                        } else {
-                            attributeA = lessonB[this.sortBy].toLowerCase()
-                            attributeB = lessonA[this.sortBy].toLowerCase()
-                        }
-                        if (attributeA < attributeB) {
-                            return -1;
-                        }
-                        if (attributeA > attributeB) {
-                            return 1;
-                        }
-                        return 0;
-                        break;
-
-                    case 'price':
-                    case 'spaces':
-                        if (this.sortOrder == 'asc') {
-                            return lessonA[this.sortBy] - lessonB[this.sortBy]
-                        } else {
-                            return lessonB[this.sortBy] - lessonA[this.sortBy]
-                        }
-                        break;
-                }
-            })
+    watch: {
+        query() {
+            this.getLessons()
         },
+        sortBy() {
+            this.getLessons()
+        },
+        sortOrder() {
+            this.getLessons()
+        }
+    },
+    computed: {
         totalCost() {
             let cost = 0
             this.cart.forEach(lesson => {
