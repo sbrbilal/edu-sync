1770b5a68f6918604ec0c193aca37a35441aaa00
Sheikh Bilal Raza <sbrbilal77@gmail.com>
2024-02-16 17:39:52 +0000

    Service Worker v6

--- service-worker.js
+++ service-worker.js
@@ -23,25 +23,25 @@ self.addEventListener('activate', function(event) {
 });
 
 // Fetch event
-self.addEventListener('fetch', function(e) {
-    console.log('Fetching:', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (cachedFile) {
-            //if the file is in the cache, retrieve it from there
-            if (cachedFile) {
-                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
-            return cachedFile;
-            } else {//if the file is not in the cache, download the file
-                return fetch(e.request).then(function (response) {
-                    return caches.open(cacheName).then(function (cache) {
-                        //add the new file to the cache
-                        cache.put(e.request, response.clone());
-                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
-                        e.request.url);
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.startsWith('https://')) {
+        event.respondWith(
+            caches.match(event.request).then(function(cachedResponse) {
+                if (cachedResponse) {
+                    console.log("[Service Worker] Resource fetched from the cache for: " + event.request.url);
+                    return cachedResponse;
+                } else {
+                    return fetch(event.request).then(function(response) {
+                        // Clone the response before caching
+                        var responseToCache = response.clone();
+                        caches.open(cacheName).then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                            console.log("[Service Worker] Resource fetched and saved in the cache for: " + event.request.url);
+                        });
                         return response;
                     });
-                });
-            }
-        })
-    ); 
+                }
+            })
+        );
+    } 
 });
